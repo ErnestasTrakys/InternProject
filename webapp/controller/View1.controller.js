d1b927d2767ddda07f2d9cff5ac697d3ad7303b2
ErnestasTrakys <ernestas.trakys@sap.com>
2022-07-07 15:02:24 +0000

    Added foundation to Search-with-filter and Routing buttons on View1.xml

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,5 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,7 +12,29 @@ sap.ui.define([
 
         return Controller.extend("visionBoardProject.internproject.controller.View1", {
             onInit: function () {
+                
+            },
+            onSubmitRoute: function () {
+                let oModel = this.getView().getModel("userSelection")
+                let selectedModelPath = oEvent.getSource().getBindingContext().sPath
+                oModel.setProperty("/selectedItemPath", selectedModelPath)
 
+                let reqSettings = {
+                    "url" : "/browse/"
+                }
+            },
+
+            onSearch: function (oEvent) {
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("newValue");
+                if (sQuery) {
+                    aFilter.push(new Filter("title", FilterOperator.Contains, sQuery));
+                }
+                var oList = this.byId("userGroup");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
             }
+
         });
+
     });
