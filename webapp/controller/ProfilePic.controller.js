086e8453e6a5fafab826096bff9bdd9fbeb37cca
Niamh Grady <niamh.grady@sap.com>
2022-08-16 10:15:32 +0000

    profile page UI

--- webapp/controller/ProfilePic.controller.js
+++ webapp/controller/ProfilePic.controller.js
@@ -11,10 +11,8 @@ sap.ui.define([
     function (BaseController) {
         "use strict";
 
-        return BaseController.extend("visionBoardProject.internproject.controller.Profile", {
+        return BaseController.extend("visionBoardProject.internproject.controller.ProfilePic", {
             onInit: function () {
-                var sPath = jQuery.sap.getModulePath("visionBoardProject.internproject", "/data/ProfileCollectionDate.json");
-                this.getView().setModel(new sap.ui.model.json.JSONModel(sPath));
                 
             },
 
